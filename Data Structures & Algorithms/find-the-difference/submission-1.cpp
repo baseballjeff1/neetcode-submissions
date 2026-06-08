cfdee882fa0e1e21f0058044a2f0fd346d87f7b7
Jeffrey Frederick <jeffreyfrederick2005@gmail.com>
2026-06-08 16:20:23 -0700

    Add: find-the-difference - submission-1

---
+++ Data Structures & Algorithms/find-the-difference/submission-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char result = '\0';
+        for (int i = 0; i < s.length(); i++) {
+            char diff = s[i] ^ t[i];
+            result ^= diff;
+        }
+
+        result = result ^ t[t.length() - 1];
+        return result;
+    }
+};
